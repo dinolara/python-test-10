e69a2dda03ae8a07340a155fd9a33d6a2fa0571c
Dilara Gareeva <159703189+dinolara@users.noreply.github.com>
2024-04-04 07:34:05 +0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,15 +1,8 @@
-text1, text2, text3 = input().split(), input().split(), input().split()
-repeat = text1 + text2 + text3
-for word in text1:
-    if word in text2 + text3:
-        repeat.remove(word)
-for word in text2:
-    if word in text1 + text3:
-        repeat.remove(word)
-for word in text3:
-    if word in text2 + text1:
-        repeat.remove(word)
+text = input().split() + input().split() + input().split()
+answer = set()
 n = 0
-for word in repeat:
-    n += len(word)
-print(f'{'#'.join(repeat)}\n{n}')
+for word in text:
+    if text.count(word) == 1:
+        answer.add(word)
+        n += len(word)
+print(f'{'#'.join(answer)}\n{n}')
